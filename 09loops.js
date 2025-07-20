493e16aed16b4bb2a3b2b9f8a8b4739aded32342
Bhaskar <negibhaskar19@gmail.com>
2025-07-20 11:00:27 +0530

    llops in js

---
+++ 09loops.js
@@ -0,0 +1,52 @@
+// const arr=[1,2,3,4,5];
+
+// const map=new Map();
+// map.set('In','India');
+// map.set('USA','United States Of America')
+
+
+// const obb={
+//     name:"Bhaskar",
+//     age:21
+// }
+// for (const item in obb) {
+//     console.log(`${item}`)
+// }
+
+const coding=["js","cpp","c","python"];
+// coding.forEach(function(item){
+//     console.log(item)
+// })
+
+
+// coding.forEach((item)=>{
+//     console.log(item)
+
+// })
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(`${item} is on ${index} of array ${arr}`)
+// })
+
+
+const myCoding=[
+    {
+        name:"Bhaskar"
+
+    },{
+        name:"Divya"
+    },
+    {
+        name:"Negi"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.name)
+})
